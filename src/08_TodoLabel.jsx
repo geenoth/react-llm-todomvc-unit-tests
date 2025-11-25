767df85de26af543c37fa8b8feafb1a53c38d36b
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 11:55:39 +0530

    Implement TodoLabel component with double-click edit functionality

--- src/08_TodoLabel.jsx
+++ src/08_TodoLabel.jsx
@@ -1,1 +1,19 @@
+import { useCallback } from "react";
+
+export function TodoLabel({ title = "", onEdit, todoId }) {
+    const handleDoubleClick = useCallback(() => {
+        if (onEdit) onEdit(todoId);
+    }, [onEdit, todoId]);
+
+    return (
+        <label
+            data-testid="todo-label"
+            onDoubleClick={handleDoubleClick}
+            role="button"
+            tabIndex={0}
+        >
+            {title}
+        </label>
+    );
+}
 
