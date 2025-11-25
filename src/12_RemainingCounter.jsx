920d2640a079ae6fcda974fc6b234e9aad0af74d
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:03:12 +0530

    Implement RemainingCounter component to display the number of remaining items

--- src/12_RemainingCounter.jsx
+++ src/12_RemainingCounter.jsx
@@ -1,1 +1,9 @@
+export function RemainingCounter({ count = 0 }) {
+    const itemText = count === 1 ? "item" : "items";
 
+    return (
+        <span className="todo-count" data-testid="remaining-counter">
+            {`${count} ${itemText} left!`}
+        </span>
+    );
+}
