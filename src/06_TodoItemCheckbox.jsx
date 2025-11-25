b80b034c5f40e763e3f13556c35e071eba53b701
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 11:35:34 +0530

    Add FilterLink_All component for filtering todos

--- src/06_TodoItemCheckbox.jsx
+++ src/06_TodoItemCheckbox.jsx
@@ -1,1 +1,22 @@
 
+import classnames from "classnames";
+
+export function FilterLink_All({ isActive = false, onFilterChange }) {
+    const handleClick = (e) => {
+        e.preventDefault();
+        onFilterChange("all");
+    };
+
+    return (
+        <li>
+            <a
+                className={classnames({ selected: isActive })}
+                href="#/"
+                data-testid="filter-link-all"
+                onClick={handleClick}
+            >
+                All
+            </a>
+        </li>
+    );
+}
