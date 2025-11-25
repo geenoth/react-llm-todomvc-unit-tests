daf0bd5f686557462babeac6dae3aaf09a5fed0d
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:01:08 +0530

    Add FilterLink_Active component with click handling for active filter

--- src/10_FilterLink_Active.jsx
+++ src/10_FilterLink_Active.jsx
@@ -1,1 +1,22 @@
 
+import classnames from "classnames";
+
+export function FilterLink_Active({ isActive = false, onFilterChange }) {
+    const handleClick = (e) => {
+        e.preventDefault();
+        onFilterChange("active");
+    };
+
+    return (
+        <li>
+            <a
+                className={classnames({ selected: isActive })}
+                href="#/active"
+                data-testid="filter-link-active"
+                onClick={handleClick}
+            >
+                Active
+            </a>
+        </li>
+    );
+}
